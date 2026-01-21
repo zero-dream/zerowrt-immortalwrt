f4925a12bb5ab7d1883b029becd15fdc74f3a6c0
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2026-01-22 01:32:49 +0800

    0

--- package/firmware/linux-firmware/aeonsemi.mk
+++
@@ -1,9 +0,0 @@
-Package/aeonsemi-as21xxx-firmware = $(call Package/firmware-default,Aeonsemi AS21xxx Ethernet PHY firmware,,LICENSE.aeonsemi)
-define Package/aeonsemi-as21xxx-firmware/install
-	$(INSTALL_DIR) $(1)/lib/firmware/aeonsemi
-	$(CP) \
-		$(PKG_BUILD_DIR)/aeonsemi/as21x1x_fw.bin \
-		$(1)/lib/firmware
-endef
-
-$(eval $(call BuildPackage,aeonsemi-as21xxx-firmware))
--- target/linux/qualcommax/ipq50xx/target.mk
+++
@@ -1,6 +0,0 @@
-SUBTARGET:=ipq50xx
-BOARDNAME:=Qualcomm Atheros IPQ50xx
-
-define Target/Description
-	Build firmware images for Qualcomm Atheros IPQ50xx based boards.
-endef
